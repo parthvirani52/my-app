6bec1a574b428fc41e6807f062b5081344e0153c
parthvirani52 <parth.virani52@gmail.com>
2022-07-21 14:17:35 +0530

    Updates

---
+++ precache-manifest.962fb66a40d5dd0a504c2ffff6b56a2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "468a7f14fb3cc3046b55",
+    "url": "/my-app/static/css/main.9ad315ff.chunk.css"
+  },
+  {
+    "revision": "468a7f14fb3cc3046b55",
+    "url": "/my-app/static/js/main.468a7f14.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/my-app/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/my-app/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "89cfe04884a91f8d06b6",
+    "url": "/my-app/static/js/runtime~main.89cfe048.js"
+  },
+  {
+    "revision": "5f9876bf84f405483d3138766a71ffe4",
+    "url": "/my-app/index.html"
+  }
+];
